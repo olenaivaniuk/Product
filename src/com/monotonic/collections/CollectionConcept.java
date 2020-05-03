93f92fb21e31147d775dafa632c6b6c27ad16209
olenaivaniuk <oivaniuk.scout24@gmail.com>
2020-05-03 15:28:33 +0200

    Initial commit

---
+++ src/com/monotonic/collections/CollectionConcept.java
@@ -0,0 +1,31 @@
+package com.monotonic.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class CollectionConcept {
+
+    public static void main(String[] args) {
+
+        Product door  = new Product("Wooden Door", 35);
+        Product floorPanel = new Product("Floor Panel", 25);
+        Product window = new Product("Glass Window", 10);
+
+        Collection<Product> products = new ArrayList<>();
+        products.add(door);
+        products.add(floorPanel);
+        products.add(window);
+
+       /*final Iterator<Product> productIterator = products.iterator();
+       while (productIterator.hasNext()) {
+           Product product = productIterator.next();
+           System.out.println(product);
+       }*/
+
+       for (Product product: products) {
+           System.out.println(product);
+       }
+
+    }
+}
